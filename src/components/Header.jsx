0c6a0ab486dc76d132459b7e108c62020809b35f
Shimouna-14 <chimene.ndeby@gmail.com>
2025-02-06 21:40:24 +0100

    add logo link to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 function Header() {
     return (
         <header>
-
+            <a href="/">
+                <h1 className="text-8xl text-center my-3 dancing-script ">Doctomap</h1>
+            </a>
         </header>
     );
 }
