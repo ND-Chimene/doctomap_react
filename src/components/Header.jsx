2f65478694c6922b8252e32d94e1630e5f1402d7
Shimouna-14 <chimene.ndeby@gmail.com>
2025-06-06 01:24:48 +0200

    ajouter la gestion de la connexion et déconnexion dans le composant Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 function Header() {
+    const token = localStorage.getItem("token");
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+    };
+
     return (
-        <header>
+        <header className="text-center">
             <a href="/">
                 <h1 className="text-8xl text-center my-3 dancing-script ">Doctomap</h1>
             </a>
+            {!token || token === "null" ? (
+                <a href="/login" className="mt-5">Se Connecter</a>
+            ) : (
+                <a href="/login" onClick={handleLogout} className="mt-5">Se deconnecter</a>
+            )}
         </header>
     );
 }
