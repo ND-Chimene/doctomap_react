2d7e5eae7b6371c6c32a8db1a533e0ac3516d81e
Shimouna-14 <chimene.ndeby@gmail.com>
2025-06-06 01:24:36 +0200

    ajouter la gestion de l'authentification avec le stockage du token dans le composant App

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Doctor from "./pages/Doctor";
 import Error from "./pages/Error";
+import Login from "./pages/Login";
 
 function App() {
+  let token = localStorage.getItem("token");
+
   return (
     <>
       <Router>
-      <Header />
+        <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/doctor/:id" element={< Doctor/>} />
+          <Route path="/doctor/:id" element={<Doctor />} />
+          <Route path="/login" element={!token ? <Login /> : <Home />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </Router>
